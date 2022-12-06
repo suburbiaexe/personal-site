c1fc33cb6c5b745e8b3e19d2942438b3ce70458c
Elizabeth <elizabeth_c_wu@brown.edu>
2022-12-06 01:51:39 -0500

    better

--- portfolio/pages/about.js
+++ portfolio/pages/about.js
@@ -16,7 +16,7 @@ export default function AboutPage() {
                     About Me
                 </h1>
                 <p>
-                    Hey! My name is [Redacted], and I'm a [redacted] at Brown University. I'm currently a Co-Director for [Redacted Club], the club responsible for [redacted]. I'm primarily a frontend web developer, but I also have experience with UI/UX and product design work (and am an avid fan of computer graphics). Most of my time is spent thinking about design and creating unique, but accessible websites, as well as gathering inspiration from posters and editorials!
+                    Hey! My name is [Redacted], and I'm a [redacted] at Brown University. I'm currently a Co-Director for [Redacted Club], the club responsible for [redacted]. I'm primarily a frontend web developer, but I also have experience with UI/UX and product design work (and am an avid fan of computer graphics). Most of my time is spent thinking about aesthetics and accessibility, as well as gathering inspiration from posters and editorials!
                 </p>
                 <p>
                     When I'm not working, I enjoy hiking, playing the New York Times Crossword, curating Spotify Playlists, playing piano, and playing The Sims 4.
